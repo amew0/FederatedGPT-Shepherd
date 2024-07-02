ebafce9cce0c8b90ed0c4353ab49ca32055a6f73
amew0 <aminekidane4@gmail.com>
2024-07-02 16:46:22 +0400

    eval pipeline with configs

--- eval_pipeline.py
+++ eval_pipeline.py
@@ -11,6 +11,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from time import time
 import gc
 import json
+import yaml
 import argparse
 import re
 from tqdm import tqdm
@@ -30,13 +31,34 @@ def inspectt(frame):
     logg("")
 
 
+def get_prompts_from_template(filepath, model_name):
+    with open(filepath, "r") as f:
+        data = yaml.safe_load(f)
+    return data[model_name]["chat_prompt"], data[model_name]["evaluation_prompt"]
+
+
+def get_tokenizer_and_model(model_name: str, cache_dir: str):
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name,
+        cache_dir=f"{cache_dir}/tokenizer",
+        pad_token_id=0,
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        cache_dir=f"{cache_dir}/model",
+        torch_dtype=torch.float16,
+        device_map="auto",
+        offload_buffers=True,
+    )
+    return tokenizer, model
+
+
 def tokenize(prompt, tokenizer):
     tokenized = tokenizer(prompt, return_tensors="pt")
     return tokenized
 
 
-def generate_and_tokenize_prompt(data_point, tokenizer):
-    prompt = """<|start_header_id|>system<|end_header_id|> {}<|eot_id|><|start_header_id|>user<|end_header_id|> {}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+def generate_and_tokenize_prompt(data_point, tokenizer, prompt=None):
     prompt = prompt.format(data_point["instruction"], data_point["input"])
     tokenized_full_prompt = tokenize(prompt, tokenizer=tokenizer)
     return tokenized_full_prompt
@@ -54,7 +76,6 @@ def extract_score(text):
 
 
 def log2json(results, json_result):
-    # Write the updated results back to the JSON file
     with open(json_result, "w") as f:
         json.dump(results, f, ensure_ascii=False, indent=4)
 
@@ -62,7 +83,7 @@ def log2json(results, json_result):
 def main(
     output_dir=f"./out",
     cache_dir=f"{ME}",
-    data_path="./data/1/eval_medical_2k.json",
+    eval_data_path="./data/1/eval_medical_2k.json",
     log_file=None,
     name="meta-llama/Meta-Llama-3-8B-Instruct",
     eval_name="meta-llama/Meta-Llama-3-8B-Instruct",
@@ -70,7 +91,6 @@ def main(
     log2wandb: bool = True,
     project="huggingface",
     entity="my-ku-org",
-    eval_prompt_path=None,
     evals_per_example=2,
 ):
     """
@@ -79,7 +99,7 @@ def main(
     Args:
     output_dir (str): Directory to save output. Default is './out'.
     cache_dir (str): Directory to load/save tokenizer/model. Default is '/dpc/kunf0097/l3-8b'.
-    data_path (str): Path to the evaluation data. Default is './data/1/eval_medical_2k.json'.
+    eval_data_path (str): Path to the evaluation data. Default is './data/1/eval_medical_2k.json'.
     log_file (str): File to dump the outputs of the evaluator. Default is {output_dir}/results_{name.split('/')[1]}_{run_id}.json.
     name (str): Model name for evaluation. Default is 'meta-llama/Meta-Llama-3-8B-Instruct'.
     eval_name (str): Model name for the evaluator. Default is 'meta-llama/Meta-Llama-3-8B-Instruct'.
@@ -87,9 +107,10 @@ def main(
     log2wandb (bool): Whether to log to Weights & Biases. Default is True.
     project (str): WandB project name. Default is huggingface.
     entity (str): WandB entity name. Default is my-ku-org.
-    eval_prompt_path (str): Path to read the prompt for the evaluator. (The prompt should place the expected placeholder first and then the generated. Don't set it to use the default one.
     evals_per_example (int): No. of times the example to be evaluated. Default is 2.
     """
+
+    chat_prompt, evaluator_prompt = get_prompts_from_template("template.yaml", name)
 
     if log2wandb and (project is None or entity is None):
         raise ValueError("Both 'project' and 'entity' must be set if 'log2wandb' is True.")
@@ -97,19 +118,9 @@ def main(
     if log_file is None:
         log_file = f"{output_dir}/results_{name.split('/')[1]}_{run_id}.json"
 
-    if eval_prompt_path is None:
-        evaluator_prompt = """<|start_header_id|>system<|end_header_id|>
-You are going to act as an LLM evaluator to rate the answer of the medical chatbot on factualness (i.e how contextually the generated output followed the expected reply). Penalize it appropriately for any hallucination, lost of context, or trailing repetition. YOUR RESPONSE IS NOTHING ELSE BUT A FLOAT FROM 0.0 - 5.0 (with format x.x). Where 0.0 indicates the context of the generated response is very far from the expected one. And 5.0 represents otherwise. AGAIN IF YOUR GENERATED ANYTHING ELSE BUT A FLOAT YOU'RE GOING TO CRUSH MY SYSTEM!!<|eot_id|><|start_header_id|>user<|end_header_id|> 
-### Expected: {}
-### Generated: {}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
-
-"""
-    else:
-        with open(eval_prompt_path, "r") as f:
-            evaluator_prompt = f.read()
-
-    # log args
     inspectt(inspect.currentframe())
+    # import sys
+    # sys.exit()
 
     start = time()
     load_dotenv()
@@ -119,102 +130,72 @@ You are going to act as an LLM evaluator to rate the answer of the medical chatb
     torch.cuda.empty_cache()
 
     logg(run_id)
-    # import sys
 
-    # sys.exit()
-
-    evaluator_tokenizer = AutoTokenizer.from_pretrained(
-        eval_name,
-        cache_dir=f"{cache_dir}/tokenizer",
-        pad_token_id=0,
+    evaluator_tokenizer, evaluator_model = get_tokenizer_and_model(
+        model_name=eval_name, cache_dir=cache_dir
     )
 
-    evaluator_model = AutoModelForCausalLM.from_pretrained(
-        eval_name,
-        cache_dir=f"{cache_dir}/model",
-        torch_dtype=torch.float16,
-        device_map="auto",
-        offload_buffers=True,
+    candidate_tokenizer, candidate_model = get_tokenizer_and_model(
+        model_name=name, cache_dir=cache_dir
     )
 
-    candidate_tokenizer = AutoTokenizer.from_pretrained(
-        name,
-        cache_dir=f"{cache_dir}/tokenizer",
-        pad_token_id=0,
-    )
-
-    candidate_model = AutoModelForCausalLM.from_pretrained(
-        name,
-        cache_dir=f"{cache_dir}/model",
-        torch_dtype=torch.float16,
-        device_map="auto",
-        offload_buffers=True,
-    )
-
-    data = load_dataset("json", data_files=data_path)
+    data = load_dataset("json", data_files=eval_data_path)
     eval_dataset = data["train"].map(
-        lambda x: generate_and_tokenize_prompt(x, candidate_tokenizer)
+        lambda x: generate_and_tokenize_prompt(x, candidate_tokenizer, chat_prompt)
     )  # not shuffled
 
     if log2wandb:
         wandb.init(project=project, entity=entity)
 
+    # generation config
+    generation_config = {
+        "max_new_tokens": 256,
+        "eos_token_id": [
+            candidate_tokenizer.eos_token_id,
+            candidate_tokenizer.convert_tokens_to_ids("<|eot_id|>"),
+        ],
+        "do_sample": True,
+        "temperature": 0.6,
+        "top_p": 0.9,
+    }
+
     results = []
     for i, example in tqdm(enumerate(eval_dataset)):
         res = None
-        example["input_ids"] = torch.LongTensor(example["input_ids"]).to(
-            candidate_model.device
-        )
-        example["attention_mask"] = torch.LongTensor(example["attention_mask"]).to(
-            candidate_model.device
-        )
-
         outputs = candidate_model.generate(
-            input_ids=example["input_ids"],
-            attention_mask=example["attention_mask"],
-            max_new_tokens=256,
-            eos_token_id=[
-                candidate_tokenizer.eos_token_id,
-                candidate_tokenizer.convert_tokens_to_ids(""),
-            ],
-            do_sample=True,
-            temperature=0.6,
-            top_p=0.9,
+            input_ids=torch.LongTensor(example["input_ids"]).to(candidate_model.device),
+            attention_mask=torch.LongTensor(example["attention_mask"]).to(
+                candidate_model.device
+            ),
+            **generation_config,
         )
 
-        response_ids = outputs[0][example["input_ids"].shape[-1] :]
+        response_ids = outputs[0][len(example["input_ids"][0]) :]
         response = candidate_tokenizer.decode(response_ids, skip_special_tokens=True)
         gt_response = example["output"]  # groundtruth
 
-        eval_tokenized = eval_prompt_tokenizer(
+        eval_prompt_tokenized = eval_prompt_tokenizer(
             response, gt_response, evaluator_tokenizer, prompt=evaluator_prompt
         )
 
         llm_scores = []
         for i in range(evals_per_example):
             eval_output = evaluator_model.generate(
-                input_ids=torch.LongTensor(eval_tokenized["input_ids"]).to(
+                input_ids=torch.LongTensor(eval_prompt_tokenized["input_ids"]).to(
                     candidate_model.device
                 ),
-                attention_mask=torch.LongTensor(eval_tokenized["attention_mask"]).to(
+                attention_mask=torch.LongTensor(eval_prompt_tokenized["attention_mask"]).to(
                     candidate_model.device
                 ),
-                max_new_tokens=128,
-                eos_token_id=[
-                    candidate_tokenizer.eos_token_id,
-                    candidate_tokenizer.convert_tokens_to_ids(""),
-                ],
-                do_sample=True,
-                temperature=0.6,
-                top_p=0.9,
+                **generation_config,
             )
 
-            eval_response = eval_output[0][eval_tokenized["input_ids"].shape[-1] :]
+            eval_response = eval_output[0][len(eval_prompt_tokenized["input_ids"][0]) :]
             llm_score = evaluator_tokenizer.decode(eval_response, skip_special_tokens=True)
             llm_scores.append(extract_score(llm_score))
 
         res = {
-            "output": gt_response,
+            "expected": gt_response,
             "generated": response,
             "llm_scores": llm_scores,
             "avg_llm_score": sum(llm_scores) / len(llm_scores),
